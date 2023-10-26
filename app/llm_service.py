1566a5f5524608bc622c1516d9a3174db68a3d58
niklas.fruehauf <niklas.fruehauf@sovanta.com>
2023-10-26 12:37:31 +0200

    fix: more logging

--- app/llm_service.py
+++ app/llm_service.py
@@ -29,6 +29,8 @@ class LLMService:
             use_flash_attention_2=False,
         )
 
+        logging.info("Model Loaded, Device Map: %s", self._llm.hf_device_map)
+
         self._tokenizer: Union[
             AutoTokenizer, LlamaTokenizerFast
         ] = AutoTokenizer.from_pretrained(llm_model)
@@ -59,6 +61,7 @@ class LLMService:
         message_buffer = []
         last_res = None
         for token in streamer:
+            logging.info("Got reply token: %s", token)
             message_buffer.append(token)
             new_res = "".join(message_buffer).rstrip("</s>").replace("  ", " ")
             if not last_res or last_res != new_res:
@@ -66,9 +69,12 @@ class LLMService:
                 last_res = new_res
 
         yield "<FINISH>"
+
+        logging.info("Finished streaming")
         try:
             future.result()
         except OutOfMemoryError as exc:
+            logging.error("OOM: %s", exc)
             loop = asyncio.get_event_loop()
             loop.stop()
             raise exc
